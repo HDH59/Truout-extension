79e42cb9959b88b41d23d40c55e9f5e197e66082
HDH59 <118896586+HDH59@users.noreply.github.com>
2025-01-27 15:35:04 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ namespace Tryout
   /* My firts extension it"s a Tryout
   */
   //% blockId = TryoutDisplayVar
-  //% block="Displayvar $x"
+  //% block="Displayvariable $x"
   //% x.min=0 x.max=9
   
     export function DisplayVar(x: number): void
